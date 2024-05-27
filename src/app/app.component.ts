487ae7b2ab95607a0fcf332beb67b1a59cd5c054
cryptomagnum <jorge_vankov@hotmail.com>
2024-05-27 12:38:52 +0200

    Genero componente nacmenu y le doy estilo Genero componente homeView en views/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, NavMenuComponent, MdbRippleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
