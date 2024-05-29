3b98a8eb8956009876714e457367365e669b9c6f
cryptomagnum <jorge_vankov@hotmail.com>
2024-05-29 16:32:16 +0200

    Diseño y agrego estilos a la primera vista (home)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes =
+    [
+        { path: '', redirectTo: 'home', pathMatch: 'full' },
+        { path: 'home', loadComponent: () => import('./views/home-view/home-view.component').then(view => view.HomeViewComponent) }
+    ];
